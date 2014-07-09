6018b0278f903f586d7d5cf0e9ca5f42f2cc9136
Tey Bannerman <tey.bannerman@gmail.com>
2014-07-09 14:07:41 +0200

    Reworked navigation bar states and interactions

--- js/subscribe.js
+++ js/subscribe.js
@@ -10,6 +10,18 @@ $(function() {
   // Sticky navigation (headroom.js)
   $('.subscribe-navbar').headroom();
 
+  // Price nav panel only appears when scrolled down past main panel
+  $(window).scroll(function(){
+    pricePanelPosition = $('.-intro').eq(0).height() + $('.-intro').eq(0).position().top;
+    if ($(window).scrollTop() > (pricePanelPosition + 50)) {
+      $('.subscribe-navbar .navbar-cta').slideDown(300);
+    }
+    else {
+      $('.subscribe-navbar .navbar-cta').slideUp(300);
+    }
+  });
+
+  // Show/hide for upgrade button section
   $('.upgrade-reveal-btn').click (function(e) {
     e.preventDefault();
     $('.upgrade-ready').slideUp();
