07040fcb8557954eb57df7a0b2354a041d9c1179
Tey Bannerman <tey.bannerman@gmail.com>
2014-07-15 03:21:07 +0200

    New subscribe index sections

--- js/subscribe.js
+++ js/subscribe.js
@@ -10,14 +10,30 @@ $(function() {
   // Sticky navigation (headroom.js)
   $('.subscribe-navbar').headroom();
 
-  // Price nav panel only appears when scrolled down past main panel
+  // Navigation scroll to anchor
+  $('.navbar-links a').click(function() {
+    $this = $(this);
+    var anchor = $this.attr('href').substring(1);
+    scrollToAnchor(anchor);
+  });
+
+  // On scroll events
   $(window).scroll(function(){
-    pricePanelPosition = $('.-intro').eq(0).height() + $('.-intro').eq(0).position().top;
-    if ($(window).scrollTop() > (pricePanelPosition + 50)) {
-      $('.subscribe-navbar .navbar-cta').slideDown(300);
-    }
-    else {
-      $('.subscribe-navbar .navbar-cta').slideUp(300);
+
+    // Fixed navigation position indicator
+    $('.subscribe-panel').each(function() {
+
+    });
+
+    // Price nav panel only appears when scrolled down past main panel
+    if ($('.-intro').length) { //check if price nav panel exists
+      pricePanelPosition = $('.-intro').eq(0).height() + $('.-intro').eq(0).position().top;
+      if ($(window).scrollTop() > (pricePanelPosition + 50)) {
+        $('.subscribe-navbar .navbar-cta').slideDown(300);
+      }
+      else {
+        $('.subscribe-navbar .navbar-cta').slideUp(300);
+      }
     }
   });
 
@@ -29,3 +45,10 @@ $(function() {
   });
 
 });
+
+
+function scrollToAnchor(aid){
+  var aTag = $("section[name='"+ aid +"']");
+  $('html,body').animate({scrollTop: aTag.offset().top - 40},'slow');
+}
+
